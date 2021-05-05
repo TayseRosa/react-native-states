238a13c6c255b16e7b3e19339628e0a91e0e64f3
Tayse Rosa <tayse_rosa@yahoo.com.br>
2021-05-05 18:37:33 -0300

    React Native States

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+/*
+  Developed by: Tayse Rosa
+  Date: 05/05/2021
+  Link: https://github.com/TayseRosa/react-native-states
+*/
+
+import React, { useState }  from 'react';
+import { StyleSheet, View, Text, Button, TextInput, } from 'react-native';
+
+export default function App(){
+
+  const [ name, setName ] = useState('');
+  const [ age, setAge ] = useState();
+  const [ info, setInfo ] = useState('Digite aqui...');
+
+  function whatsName(){
+    setName('Tayse Rosa')
+  }
+
+  function whatsAge(){
+    setAge('33');
+  }
+  
+
+  return(
+    <>
+      <View style={styles.container}>
+        <Text style={styles.headerTitle} > REACT NATIVE </Text>
+        <Text style={styles.headerSubtitle} > STATES </Text>
+      </View>
+
+      <View style={styles.container} >
+        <Button title="Whats my name? Click here" onPress={whatsName}  />
+          <Text style={styles.answer} > {name} </Text>
+      </View>
+
+      <View style={styles.container} >
+        <Button title="Whats is my age? Click here" onPress={whatsAge}  />
+          <Text style={styles.answer} > {age} </Text>
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.headerSubtitle} >Insert infos</Text>
+          <TextInput style={styles.textInput}  value={info}  onChangeText={i=>setInfo(i)} />  
+        <Text style={styles.headerSubtitle} > Você digitou: {info} </Text>
+      </View>
+      
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1, 
+    justifyContent:'center',
+    alignItems:'center',
   },
+  headerTitle:{
+    fontSize:24
+  },
+  headerSubtitle:{
+    fontSize:20
+  },
+  textInput:{
+    width:200,
+    height:50,
+    borderWidth:3,
+    borderStyle: 'solid', 
+    borderColor:'#b656a1',
+    paddingLeft:20,
+    paddingRight:20,
+    color:'#b656a1',
+    fontSize:20
+  },
+  answer:{
+    fontSize:20,
+    color: '#b656a1'
+  }
 });
---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center"> REACT NATIVE (BEGGINER) - PROPS </h1>
+
+#Índice
+- [About](#-about)
+- [Technologies used](#-tecnologias-utilizadas)
+- [How to use](#-how-to-use)
+
+## ➡️ About
+<b>Portugues:</b>
+O projeto foi desenvolvido com a intenção de ser uma **base de conhecimento** para consultas de desenvolvedores iniciantes na área de **React Native**, e também para minhas futuras consultas rápidas.
+
+<b>English:</b>
+The project was developed with the intention of being a ** knowledge base ** for queries from developers beginning in the area of ** React Native **, and also for my future quick queries.
+
+---
+
+## 🚀 Technologies used
+<b>Portugues:</b>
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+<b>English:</b>
+The project was developed using the following technologies.
+
+- Expo
+- React Native
+- States
+
+---
+
+## 📥 How to use
+```js
+
+    //Clone the repository
+    $ git clone https: github.com/TayseRosa/react-native-states.git
+
+    //Enter the directory
+    $ cd rnStates
+
+    //Install the dependencies the project
+    $ yarn install
+
+    //Start the project
+    $ yarn start
+
+``` 
+
+---
+<h4 align="center"> Developed by 🚀 Tayse Rosa 🌸 </h4>
